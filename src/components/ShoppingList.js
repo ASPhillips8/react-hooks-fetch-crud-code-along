6111fdd5aeb4cbd8e1ebcb2053eca72b6e68b1a0
Anthony Phillips <anthony.s.phillips888@gmail.com>
2024-06-18 09:26:28 -0400

    Fixed spacing and remove console logs

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,11 @@
-import React, { useEffect, useState } from "react";
-import ItemForm from "./ItemForm";
-import Filter from "./Filter";
-import Item from "./Item";
+import React, { useEffect, useState } from "react"
+import ItemForm from "./ItemForm"
+import Filter from "./Filter"
+import Item from "./Item"
 
 function ShoppingList() {
-  const [selectedCategory, setSelectedCategory] = useState("All");
-  const [items, setItems] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState("All")
+  const [items, setItems] = useState([])
 
   useEffect(() => {
     fetch("http://localhost:4000/items")
@@ -19,31 +19,28 @@ function ShoppingList() {
   }
 
   function handleUpdateItem(updatedItemData) {
-     const updatedItems = items.map((item) => {
+    const updatedItems = items.map((item) => {
       if (item.id === updatedItemData.id) {
         return updatedItemData
       } else {
         return item
       }
-     })
-     setItems(updatedItems)
+    })
+    setItems(updatedItems)
   }
 
   function handleAddItems(newItem) {
-    console.log("in Shopping", newItem)
     setItems([...items, newItem])
   }
 
-
   function handleCategoryChange(category) {
-    setSelectedCategory(category);
+    setSelectedCategory(category)
   }
 
   const itemsToDisplay = items.filter((item) => {
-    if (selectedCategory === "All") return true;
-
-    return item.category === selectedCategory;
-  });
+    if (selectedCategory === "All") return true
+    return item.category === selectedCategory
+  })
 
   return (
     <div className="ShoppingList">
@@ -63,7 +60,7 @@ function ShoppingList() {
         ))}
       </ul>
     </div>
-  );
+  )
 }
 
-export default ShoppingList;
+export default ShoppingList
